ce50a8875d1c05febc19c043054584e365768317
Agus Prasetyo <agusprasetyo1889@gmail.com>
2023-06-27 15:57:24 +0700

    add fortify for authentification

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.custom')
 
+@section('title', 'Title')
+
 @section('content')
 <div class="row">
 	<div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
@@ -11,34 +13,42 @@
 			<div class="card-header"><h4>Login</h4></div>
 
 			<div class="card-body">
-				<form method="POST" action="#" class="needs-validation" novalidate="">
-				<div class="form-group">
-					<label for="email">Email</label>
-					<input id="email" type="email" class="form-control" name="email" tabindex="1" required autofocus>
-					<div class="invalid-feedback">
-						Please fill in your email
-					</div>
-				</div>
+				<form method="POST" action="{{ route('login') }}" class="needs-validation" novalidate="">
+					@csrf
 
-				<div class="form-group">
-					<div class="d-block">
-						<label for="password" class="control-label">Password</label>
-					<div class="float-right">
-						<a href="{{ route('forgot') }}" class="text-small">
-							Forgot Password?
-						</a>
+					<div class="form-group">
+						<label for="email">Email</label>
+						<input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" tabindex="1" autofocus>
+						
+						@error('email')
+							<div class="invalid-feedback">
+								{{ $message }}
+							</div>
+						@enderror
 					</div>
+
+					<div class="form-group">
+						<div class="d-block">
+							<label for="password" class="control-label">Password</label>
+						<div class="float-right">
+							{{-- <a href="{{ route('forgot') }}" class="text-small"> --}}
+							<a href="#" class="text-small">
+								Forgot Password?
+							</a>
+						</div>
+						</div>
+						<input id="password" type="password" class="form-control @error('email') is-invalid @enderror" name="password" tabindex="2">
+						@error('password')
+							<div class="invalid-feedback">
+								{{ $message }}
+							</div>
+						@enderror
 					</div>
-					<input id="password" type="password" class="form-control" name="password" tabindex="2" required>
-					<div class="invalid-feedback">
-						please fill in your password
+					<div class="form-group">
+						<button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
+						Login
+						</button>
 					</div>
-				</div>
-				<div class="form-group">
-					<button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
-					Login
-					</button>
-				</div>
 				</form>
 			</div>
 		</div>
