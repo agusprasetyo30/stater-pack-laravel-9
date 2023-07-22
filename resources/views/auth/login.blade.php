7499f38668441091dfbdfec1ea2cffb9a4456df0
Agus Prasetyo <agusprasetyo1889@gmail.com>
2023-07-22 22:52:40 +0700

    fix feature reset pasword, register, verify, forgot password

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
 <div class="row">
 	<div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
 		<div class="login-brand">
-		<img src="../assets/img/stisla-fill.svg" alt="logo" width="100" class="shadow-light rounded-circle">
+			<img src="../assets/img/stisla-fill.svg" alt="logo" width="100" class="shadow-light rounded-circle">
 		</div>
 
 		<div class="card card-primary">
@@ -31,8 +31,7 @@
 						<div class="d-block">
 							<label for="password" class="control-label">Password</label>
 						<div class="float-right">
-							{{-- <a href="{{ route('forgot') }}" class="text-small"> --}}
-							<a href="#" class="text-small">
+							<a href="{{ route('password.request') }}" class="text-small">
 								Forgot Password?
 							</a>
 						</div>
